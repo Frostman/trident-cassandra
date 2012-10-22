3b5c367d7cba8b67d9daa650f1e2d1ff2db65998
Sergey Lukjanov <me@frostman.ru>
2012-10-22 17:46:04 +0400

    groupid changed

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject frostman/trident-cassandra "0.0.1-wip1"
+(defproject trident-cassandra "0.0.1-wip1"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
